88543e9cca0096a52d02ccaef5769a18b28d3d92
Amal168 <amalsubala2024@gmail.com>
2024-12-19 11:55:28 +0530

    19-12-2024

--- lib/MVVM/view/chats.dart
+++ lib/MVVM/view/chats.dart
@@ -16,142 +16,142 @@ class _ChatsState extends State<Chats> {
   final inchats = TextEditingController();
   final emailController = TextEditingController();
   final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: greyColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        actions: [IconButton(
+            onPressed: () {
+              Firebasesurvices().signout();
+            },
+            icon: Icon(Icons.logout)),],
         title: Text(
           "Chats",
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        leading: IconButton(
-            onPressed: () {
-              Firebasesurvices().signout();
-            },
-            icon: Icon(Icons.logout)),
+        
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("user").snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final docs = snapshot.data!.docs;
-          if (docs == null || docs.isEmpty) {
-            return Center(
-              child: Text("there is no chats avalable"),
-            );
-          }
-          return ListView.builder(
-            shrinkWrap: true,
-            reverse: false,
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final data = docs[index];
-              return Padding(
-                padding: const EdgeInsets.all(10),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      trailing: IconButton(
-                          onPressed: () async {
-                            await Firebasepage().deleteData(documentId: data.id);
-                          },
-                          icon: Icon(Icons.delete)),
-                      title: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data['email'].toString(),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance.collection("user").snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                final docs = snapshot.data!.docs;
+                if (docs == null || docs.isEmpty) {
+                  return Center(
+                    child: Text("There are no chats available."),
+                  );
+                }
+                return ListView.builder(
+                  shrinkWrap: true,
+                  reverse: false,
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    final data = docs[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: CircleAvatar(),
+                            trailing: IconButton(
+                                onPressed: () async {
+                                  await Firebasepage().deleteData(documentId: data.id);
+                                },
+                                icon: Icon(Icons.delete)),
+                            title: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(data['email'].toString()),
+                                Text(data['chats'].toString()),
+                                
+                              ],
+                            ),
                           ),
-                          Text(
-                            data['chats'].toString(),
-                          ),
-                        ],
+                          
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:
-          Container(
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+           Row(
+             crossAxisAlignment: CrossAxisAlignment.end,
+             children: [
+                         Expanded(
+                           child: Padding(
+                             padding: const EdgeInsets.all(10.0),
+                             child: Form(
+                               key: formkey,  
+                               child: Container(
+            height: 60,
             decoration: BoxDecoration(
-              color: Colors.white
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(15)),
+            child: TextFormField(
+              controller: inchats,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter something";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                hintText: "Chats",
+                hintStyle: TextStyle(fontSize: 20),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(15)),
+                fillColor: Colors.white,
+                filled: true,
+              ),
             ),
-            margin: EdgeInsets.all(3),
-            height: 70,
-            child: Row(crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      height: 90,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                      child: TextFormField(
-                          controller: inchats,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "enter Something";
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            hintText: "Chats",
-                            hintStyle: TextStyle(fontSize: 20),
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(15)),
-                            fillColor: Colors.black12,
-                            filled: true,
-                          )),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: MaterialButton(
-                      height: 60,
-                      color: Colors.black12,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      onPressed: () async {
-                        if (formkey.currentState!.validate()) {
-                          try {
-                            await Firebasepage().createdata(
-                                FirebaseAuth.instance.currentUser!.email.toString(),
-                                inchats.text);
-                            inchats.clear();
-                          } catch (e) {
-                            print('Error: $e');
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(SnackBar(content: Text("Error: $e")));
-                          }
-                        }
-                      },
-                      child: Icon(Icons.send)),
-                ),
-              ],
-            ),
-            
-          
-    
+                               ),
+                             ),
+                           ),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: MaterialButton(
+                             height: 60,
+                             color: Colors.black12,
+                             shape: RoundedRectangleBorder(
+                               borderRadius: BorderRadius.circular(10),
+                             ),
+                             onPressed: () async {
+                               if (formkey.currentState!.validate()) {
+            try {
+              await Firebasepage().createdata(
+                  FirebaseAuth.instance.currentUser!.email.toString(),
+                  inchats.text);
+              inchats.clear();
+            } catch (e) {
+              print('Error: $e');
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text("Error: $e")));
+            }
+                               }
+                             },
+                             child: Icon(Icons.send),
+                           ),
+                         ),
+             ],
+           ),
+        ],
       ),
     );
   }
